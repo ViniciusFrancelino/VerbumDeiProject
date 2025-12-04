87a79451b5c9c1c0cc441b6e1083279792ef6840
ViniciusFrancelino <vinicius.fsilva@sptech.school>
2025-12-04 13:34:51 -0300

    UPDATE: KPIs Finalizadas

--- develop/src/models/dashModel.js
+++ develop/src/models/dashModel.js
@@ -3,7 +3,19 @@ var database = require("../database/config");
 function plotarKpis(idUsuario) {
 
     var instrucaoSql = ``;
-    instrucaoSql = `select SUM(pontuacao) from quizResultado where idUsuario = ${idUsuario};`;
+
+    instrucaoSql = `SELECT 
+    (SELECT tipoQuiz 
+     FROM quizResultado 
+     WHERE idUsuario = ${idUsuario}
+     GROUP BY tipoQuiz 
+     ORDER BY COUNT(*) DESC 
+     LIMIT 1) AS quizFavoritos,
+    ROUND(AVG(pontuacao) * 100 / 10, 2) AS porcentual,
+    SUM(idUsuario) AS TentativasRealizadas
+    FROM quizResultado
+    WHERE idUsuario = ${idUsuario};
+    `;
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
