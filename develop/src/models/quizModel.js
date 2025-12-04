522714e274e534606e56e769df3a77708d4c75bf
ViniciusFrancelino <vinicius.fsilva@sptech.school>
2025-12-04 09:12:27 -0300

    UPDATE

--- develop/src/models/quizModel.js
+++ develop/src/models/quizModel.js
@@ -1,8 +1,9 @@
 var database = require("../database/config");
 
-function registrar(idUsuario, tentativa, tipoQuiz){
-    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", idUsuario, tentativa, tipoQuiz);
-    var instrucaoSql = `INSERT INTO quizResultados (idUsuario, pontuacao, tipoQuiz) VALUES ('${idUsuario}', '${pontuacao}', '${tipoQuiz}');`
+function registrar(idUsuario, pontuacao, tipoQuiz){
+    console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function entrar(): ", idUsuario, pontuacao, tipoQuiz);
+    var instrucaoSql = '';
+    instrucaoSql = `INSERT INTO quizResultados (idUsuario, pontuacao, tipoQuiz) VALUES ('${idUsuario}', '${pontuacao}', '${tipoQuiz}');`
 
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
